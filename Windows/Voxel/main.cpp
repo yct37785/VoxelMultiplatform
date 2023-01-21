99f51fb3534df391191a7aa13eb1e73840b581a6
Yctan Laptop (ASUS TUF) <yiechertan@gmail.com>
2023-01-21 18:42:31 +0800

    New mem leak macro. Only detect once program terminates and not at end of main().

--- Windows/Voxel/main.cpp
+++ Windows/Voxel/main.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
+
 	OSEngine::instance()->Init();
 	OSEngine::instance()->Run();
 
 	// delete all instances
 	delete OSEngine::instance();
 
-	// display memory leak report
-	_CrtDumpMemoryLeaks();
+	// display memory leak report (on program termination)
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF);
 }
